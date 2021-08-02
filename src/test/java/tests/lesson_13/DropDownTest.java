e044914b92f4c5a924e0d28448a7846f1c65bb68
Anna <giannagrale@gmail.com>
2021-08-02 15:56:23 +0300

    DropDown.java refactor, selectByIndex added

--- src/test/java/tests/lesson_13/DropDownTest.java
+++ src/test/java/tests/lesson_13/DropDownTest.java
@@ -23,7 +23,7 @@ public class DropDownTest extends BaseTest {
     }
 
     @Test
-    public void dropDownByIndexTest() throws InterruptedException {
+    public void dropDownByIndexTest()   {
         LoginStep loginStep = new LoginStep(driver);
         loginStep.login(properties.getUsername(), properties.getPassword());
         DashboardPage dashboardPage = new DashboardPage(driver, true);
@@ -32,6 +32,5 @@ public class DropDownTest extends BaseTest {
         dropDown.openMenu();
         dropDown.selectByIndex(10);
         waits.waitForVisibility(By.className("dialog-logo"));
-
     }
 }
